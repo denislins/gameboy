1b8443c7c81c2de5300d67d699a033388fc0eb39
Denis Lins <denis.lins@shopify.com>
2019-06-14 17:19:37 -0300

    I broke the PPU while trying to implement scrolling

--- emulator/gpu/Ppu.js
+++ emulator/gpu/Ppu.js
@@ -8,48 +8,63 @@ export default class Ppu {
     const pixels = [];
 
     for (let row = 0; row < 144; row++) {
-      pixels.push(...this.renderRow(row));
+      const actualRow = this.mmu.registers.read('scy') + row;
+
+      for (let column = 0; column < 160; column++) {
+        const actualColumn = this.mmu.registers.read('scx') + column;
+        const pixel = this.renderPixel(actualRow, actualColumn);
+
+        pixels.push(pixel);
+      }
     }
 
     return pixels;
   }
 
-  renderRow(row) {
-    const pixels = [];
+  renderPixel(row, column) {
+    const tileIdentityOffset = Math.floor(row / 8) * 32 + Math.floor(column / 8);
+    const tileIdentityAddress = this.controller.getBackgroundBaseAddress() + tileIdentityOffset;
 
-    const tileIdentityOffset = Math.floor(row / 8) * 32;
+    const tileNumber = this.mmu.read(tileIdentityAddress);
+    const tileBaseAddress = this.calculateTileBaseAddress(tileNumber);
 
-    const baseIdentityAddress = this.controller.getBackgroundBaseAddress() + tileIdentityOffset;
     const tileAddressOffset = (row % 8) * 2;
+    const tileRowAddress = tileBaseAddress + tileAddressOffset;
 
-    for (let tile = 0; tile < 20; tile++) {
-      const tileNumber = this.mmu.read(baseIdentityAddress + tile);
-      const tileBaseAddress = this.controller.getTilesBaseAddress() + 16 * tileNumber;
+    const internalColor = this.calculateInternalColor(tileRowAddress, row % 8);
+    const displayColor = this.mapDisplayColor(internalColor);
 
-      const tileRowAddress = tileBaseAddress + tileAddressOffset;
-      const tileRowPixels = this.renderTileRow(tileRowAddress);
-
-      pixels.push(...tileRowPixels);
-    }
-
-    return pixels;
+    return displayColor;
   }
 
-  renderTileRow(baseAddress) {
-    const pixels = [];
+  calculateTileBaseAddress(tileNumber) {
+    const tileBaseAddress = this.controller.getTilesBaseAddress();
 
+    if (tileBaseAddress === 0x8800) {
+      return tileBaseAddress + (tileNumber ^ 0x80) * 16;
+    }
+
+    return tileBaseAddress + tileNumber * 16;
+  }
+
+  calculateInternalColor(baseAddress, bit) {
     const byte1 = this.mmu.read(baseAddress);
     const byte2 = this.mmu.read(baseAddress + 1);
 
-    for (let i = 0; i < 8; i++) {
-      const mask = 1 << (7 - i);
+    const mask = 1 << (7 - bit);
 
-      const bit1 = (byte1 & mask) >> (7 - i);
-      const bit2 = (byte2 & mask) >> (7 - i);
+    const bit1 = (byte1 & mask) >> (7 - bit);
+    const bit2 = (byte2 & mask) >> (7 - bit);
 
-      pixels.push((bit2 << 1) | bit1);
-    }
+    return (bit2 << 1) | bit1;
+  }
 
-    return pixels;
+  mapDisplayColor(color) {
+    return color;
+
+    const pallete = this.mmu.registers.read('bgp');
+    const mask = 11 << (color * 2);
+
+    return (pallete & mask) >> (color * 2);
   }
 }
