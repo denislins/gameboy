75b9a68113799c9bc66eb82eb667b066ab8552c1
Denis Lins <denis.lins@shopify.com>
2019-06-15 01:28:47 -0300

    Window should now be working

--- emulator/gpu/Ppu.js
+++ emulator/gpu/Ppu.js
@@ -8,11 +8,14 @@ export default class Ppu {
     const pixels = [];
 
     for (let row = 0; row < 144; row++) {
-      const actualRow = this.mmu.registers.read('scy') + row;
-
       for (let column = 0; column < 160; column++) {
-        const actualColumn = this.mmu.registers.read('scx') + column;
-        const pixel = this.renderPixel(actualRow, actualColumn);
+        let pixel;
+
+        if (this.isWindowEnabled(row, column)) {
+          pixel = this.renderWindowPixel(row, column);
+        } else {
+          pixel = this.renderBackgroundPixel(row, column);
+        }
 
         pixels.push(pixel);
       }
@@ -21,12 +24,50 @@ export default class Ppu {
     return pixels;
   }
 
-  renderPixel(row, column) {
-    const tileNumber = this.calculateTileNumber(row, column);
-    const tileBaseAddress = this.calculateTileBaseAddress(tileNumber);
+  isWindowEnabled(row, column) {
+    if (!this.controller.isWindowEnabled()) {
+      return false;
+    } else if (this.mmu.registers.read('wy') > row) {
+      return false
+    } else if ((this.mmu.registers.read('wx') - 7) > column) {
+      return false;
+    }
+
+    return true;
+  }
+
+  renderWindowPixel(row, column) {
+    const actualRow = row - this.mmu.registers.read('wy');
+    const windowY = this.mmu.registers.read('wx');
+
+    let actualColumn;
+
+    if (column >= windowY) {
+      actualColumn = column - windowY + 7;
+    } else {
+      actualColumn = this.mmu.registers.read('scx') + column;
+    }
+
+    const windowBaseAddress = this.controller.getWindowBaseAddress();
+
+    return this.renderPixel(windowBaseAddress, actualRow, actualColumn);
+  }
+
+  renderBackgroundPixel(row, column) {
+    const actualRow = this.mmu.registers.read('scy') + row;
+    const actualColumn = this.mmu.registers.read('scx') + column;
+
+    const backgroundBaseAddress = this.controller.getBackgroundBaseAddress();
+
+    return this.renderPixel(backgroundBaseAddress, actualRow, actualColumn);
+  }
+
+  renderPixel(baseAddress, row, column) {
+    const tileNumber = this.calculateTileNumber(baseAddress, row, column);
+    const tileAddress = this.calculateTileAddress(tileNumber);
 
     const tileAddressOffset = (row % 8) * 2;
-    const tileRowAddress = tileBaseAddress + tileAddressOffset;
+    const tileRowAddress = tileAddress + tileAddressOffset;
 
     const internalColor = this.calculateInternalColor(tileRowAddress, column % 8);
     const displayColor = this.mapDisplayColor(internalColor);
@@ -34,14 +75,14 @@ export default class Ppu {
     return displayColor;
   }
 
-  calculateTileNumber(row, column) {
+  calculateTileNumber(baseAddress, row, column) {
     const tileIdentityOffset = Math.floor(row / 8) * 32 + Math.floor(column / 8);
-    const tileIdentityAddress = this.controller.getBackgroundBaseAddress() + tileIdentityOffset;
+    const tileIdentityAddress = baseAddress + tileIdentityOffset;
 
     return this.mmu.read(tileIdentityAddress);
   }
 
-  calculateTileBaseAddress(tileNumber) {
+  calculateTileAddress(tileNumber) {
     const tileBaseAddress = this.controller.getTilesBaseAddress();
 
     if (tileBaseAddress === 0x8800) {
